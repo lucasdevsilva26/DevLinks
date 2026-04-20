d434038267ac1123cca4658f28953848dd58e29d
lucasferreirasilva26 <lucasferreirasilvaadry@gmail.com>
2026-04-20 11:23:59 -0300

    adicionando README

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function togglemode() {
+  const html = document.documentElement;
+  html.classList.toggle("light");
+const img = document.querySelector("#profile img");
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "/assets/profile.png");
+  } else {
+    img.setAttribute("src", "/assets/profile-dark.png");
+  }
+}
